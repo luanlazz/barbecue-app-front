3931228c923c26f734e2a0db8c0b29625418a687
luan.lazzari <luanlazzari@hotmail.com>
2020-08-01 15:13:09 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
     '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
