154f86eb6a4ee909a814ae9d53428e6a1aaaa019
luan.lazzari <luanlazzari@hotmail.com>
2020-08-03 01:47:53 -0300

    test: ensure Input Component remove readOnly on focus

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent } from '@testing-library/react'
 import Input from './input'
 import Context from '@/presentation/contexts/form/form-context'
+import faker from 'faker'
 
-const makeSut = (): RenderResult => {
+const makeSut = (fieldName: string): RenderResult => {
   return render(
     <Context.Provider value={{ state: {} }}>
-      <Input name='field' />
+      <Input name={fieldName} />
     </Context.Provider>)
 }
 
 describe('Input Component', () => {
   test('Should begin with readOnly', () => {
-    const sut = makeSut()
-    const input = sut.getByTestId('field') as HTMLInputElement
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const input = sut.getByTestId(field) as HTMLInputElement
     expect(input.readOnly).toBeTruthy()
+  })
+
+  test('Should remove readOnly on focus', () => {
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const input = sut.getByTestId(field) as HTMLInputElement
+    fireEvent.focus(input)
+    expect(input.readOnly).toBeFalsy()
   })
 })
