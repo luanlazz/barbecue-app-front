73b6ab1be8b6dd9881ad69e74bcb2e277a5dfa46
luan.lazzari <luanlazzari@hotmail.com>
2020-08-04 20:59:12 -0300

    feat: ensure RemoteLoadBarbecueList returns a list on 200

--- src/data/usecases/load-barbecue-list/remote-load-barbecue-list.ts
+++ src/data/usecases/load-barbecue-list/remote-load-barbecue-list.ts
@@ -1,17 +1,19 @@
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
 import { UnexpectedError } from '@/domain/errors'
+import { BarbecueModel } from '@/domain/models'
+import { LoadBarbecueList } from '@/domain/usecases/load-barbecue-list'
 
-export class RemoteLoadBarbecueList {
+export class RemoteLoadBarbecueList implements LoadBarbecueList {
   constructor (
     private readonly url: string,
     private readonly httpGetClient: HttpGetClient
   ) {}
 
-  async loadAll (): Promise<void> {
+  async loadAll (): Promise<BarbecueModel[]> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       default: throw new UnexpectedError()
     }
   }
