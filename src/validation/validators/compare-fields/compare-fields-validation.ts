89853062ebea8f42d840d6bafd3896ab38f47e3a
luan.lazzari <luanlazzari@hotmail.com>
2020-08-03 21:49:02 -0300

    feat: ensure CompareFieldsValidation returns falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -8,6 +8,6 @@ export class CompareFieldsValidation implements FieldValidation {
   ) {}
 
   validate (value: string): Error {
-    return new InvalidFieldError(this.field)
+    return value !== this.valueToCompare ? new InvalidFieldError(this.field) : null
   }
 }
