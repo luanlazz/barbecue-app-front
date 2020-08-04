2f2d62757c8bb79712d1839074e537fd22d20e30
luan.lazzari <luanlazzari@hotmail.com>
2020-08-04 20:37:39 -0300

    feat: ensure RemoteLoadBarbecueList throws UnexpectedError on 403

--- src/data/usecases/load-barbecue-list/remote-load-barbecue-list.ts
+++ src/data/usecases/load-barbecue-list/remote-load-barbecue-list.ts
@@ -1,15 +1,18 @@
-import { LoadBarbecueList } from '@/domain/usecases/load-barbecue-list'
-import { BarbecueModel } from '@/domain/models'
-import { HttpGetClient } from '@/data/protocols/http'
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
+import { UnexpectedError } from '@/domain/errors'
 
-export class RemoteLoadBarbecueList implements LoadBarbecueList {
+export class RemoteLoadBarbecueList {
   constructor (
     private readonly url: string,
     private readonly httpGetClient: HttpGetClient
   ) {}
 
-  async loadAll (): Promise<BarbecueModel[]> {
-    await this.httpGetClient.get({ url: this.url })
-    return null
+  async loadAll (): Promise<void> {
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
+      default: throw new UnexpectedError()
+    }
   }
 }
