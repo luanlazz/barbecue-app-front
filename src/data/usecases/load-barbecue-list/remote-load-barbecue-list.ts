7af14bf53b08499e3fef8607efa749fd11b7f218
luan.lazzari <luanlazzari@hotmail.com>
2020-08-04 21:02:23 -0300

    feat: ensure RemoteLoadBarbecueList return empty list on 204

--- src/data/usecases/load-barbecue-list/remote-load-barbecue-list.ts
+++ src/data/usecases/load-barbecue-list/remote-load-barbecue-list.ts
@@ -14,6 +14,7 @@ export class RemoteLoadBarbecueList implements LoadBarbecueList {
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: return httpResponse.body
+      case HttpStatusCode.noContent: return []
       default: throw new UnexpectedError()
     }
   }
