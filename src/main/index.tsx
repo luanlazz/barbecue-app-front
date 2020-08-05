3b2ca44010538f1360d0e31bd3bc72e9bfe2e1ca
luan.lazzari <luanlazzari@hotmail.com>
2020-08-05 18:09:01 -0300

    refactor: move Router to main layer

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { Router } from '@/presentation/components'
-import { makeLogin } from './factories/pages/login/login-factory'
-import { makeSignUp } from './factories/pages/signup/signup-factory'
+import Router from '@/main/routes/router'
 import '@/presentation/styles/global.scss'
 
 ReactDom.render(
-  <Router
-    makeLogin={makeLogin}
-    makeSignUp={makeSignUp}
-  />,
+  <Router />,
   document.getElementById('main')
 )
--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,21 +1,18 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { makeLogin } from '@/main/factories/pages/login/login-factory'
+import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
 import { Spinner } from '@/presentation/components'
 import BarbecueList from '@/presentation/pages/barbecue-list/barbecue-list'
 
-type Factory = {
-  makeLogin: React.FC
-  makeSignUp: React.FC
-}
-
-const Router: React.FC<Factory> = (factory: Factory) => {
+const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<Spinner />}>
         <Switch>
-          <Route path='/login' exact component={factory.makeLogin} />
-          <Route path='/signup' exact component={factory.makeSignUp} />
-          <Route path='/barbecues' exact component={BarbecueList} />
+          <Route path='/login' exact component={makeLogin} />
+          <Route path='/signup' exact component={makeSignUp} />
+          <Route path='/' exact component={BarbecueList} />
         </Switch>
       </Suspense>
     </BrowserRouter>
