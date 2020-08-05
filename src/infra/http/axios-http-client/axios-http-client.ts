fb1865db3fd9d6b550eea0c8aa0f00885b45d67d
luan.lazzari <luanlazzari@hotmail.com>
2020-08-04 21:40:06 -0300

    refactor: move duplicated code to helper method

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -5,24 +5,29 @@ import axios, { AxiosResponse } from 'axios'
 export class AxiosHttpClient implements HttpPostClient, HttpGetClient {
   async post (params: HttpPostParams): Promise<HttpResponse> {
     let axiosResponse: AxiosResponse
+
     try {
       axiosResponse = await axios.post(params.url, params.body)
     } catch (error) {
       axiosResponse = error.response
     }
-    return {
-      statusCode: axiosResponse.status,
-      body: axiosResponse.data
-    }
+
+    return this.adapt(axiosResponse)
   }
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
     let axiosResponse: AxiosResponse
+
     try {
       axiosResponse = await axios.get(params.url)
     } catch (error) {
       axiosResponse = error
     }
+
+    return this.adapt(axiosResponse)
+  }
+
+  private adapt (axiosResponse: AxiosResponse): HttpResponse {
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
