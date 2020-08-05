03122dd843c48ad36c1a4603f06ff4ce960d9dc1
luan.lazzari <luanlazzari@hotmail.com>
2020-08-05 01:28:25 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/barbecue-list/barbecue-list.tsx
+++ src/presentation/pages/barbecue-list/barbecue-list.tsx
@@ -31,6 +31,47 @@ const BarbecueList: React.FC = () => {
               </div>
             </div>
           </li>
+
+          <li>
+            <div className={Styles.barbecueContent}>
+              <time>
+                <span className={Styles.date}>01/12</span>
+              </time>
+              <span className={Styles.description}>Niver do Gui</span>
+
+              <div className={Styles.totals}>
+                <div className={Styles.people}>
+                  <IconPeople />
+                  <span className={Styles.num}>15</span>
+                </div>
+                <div className={Styles.money}>
+                  <IconMoney />
+                  <span className={Styles.valueTotal}>R$ 280</span>
+                </div>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className={Styles.barbecueContent}>
+              <time>
+                <span className={Styles.date}>01/12</span>
+              </time>
+              <span className={Styles.description}>Niver do Gui</span>
+
+              <div className={Styles.totals}>
+                <div className={Styles.people}>
+                  <IconPeople />
+                  <span className={Styles.num}>15</span>
+                </div>
+                <div className={Styles.money}>
+                  <IconMoney />
+                  <span className={Styles.valueTotal}>R$ 280</span>
+                </div>
+              </div>
+            </div>
+          </li>
+
         </ul>
 
       </div>
