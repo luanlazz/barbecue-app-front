f6b99e04ec9d5b9a4d42007c8c869abb1cfadbec
luan.lazzari <luanlazzari@hotmail.com>
2020-08-05 20:23:30 -0300

    feat: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -7,3 +7,7 @@ export const setCurrentAccountAdapter = (account: AccountModel): void => {
 
   makeLocalStorageAdapter().set('account', account)
 }
+
+export const getCurrentAccountAdapter = (): AccountModel => {
+  return makeLocalStorageAdapter().get('account')
+}
