025fa594aa8ad6d853f657d5b0cad5042e75a238
luan.lazzari <luanlazzari@hotmail.com>
2020-08-06 01:05:57 -0300

    test: ensure BarbecueList render BarbecueItems on success

--- src/presentation/pages/barbecue-list/barbecue-list.spec.tsx
+++ src/presentation/pages/barbecue-list/barbecue-list.spec.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { BarbecueList } from '@/presentation/pages'
 import { LoadBarbecueList } from '@/domain/usecases/load-barbecue-list'
 import { BarbecueModel } from '@/domain/models'
+import { mockBarbecuesModel } from '@/domain/test'
 
 class LoadBarbecueListSpy implements LoadBarbecueList {
-  callsCount: number=0
+  callsCount: number = 0
+  barbecues = mockBarbecuesModel()
 
   async loadAll (): Promise<BarbecueModel[]> {
     this.callsCount++
-    return []
+    return this.barbecues
   }
 }
 
@@ -28,14 +30,23 @@ const makeSut = (): SutTypes => {
 }
 
 describe('BarbecueList Component', () => {
-  test('Should present 3 empty items on start', () => {
+  test('Should present 3 empty items on start', async () => {
     makeSut()
     const barbecueList = screen.getByTestId('barbecue-list')
-    expect(barbecueList.querySelectorAll('li:empty').length).toBe(3)
+    expect(barbecueList.querySelectorAll('li:empty')).toHaveLength(3)
+    await waitFor(() => barbecueList)
   })
 
-  test('Should call LoadBarbecueList', () => {
+  test('Should call LoadBarbecueList', async () => {
     const { loadBarbecueListSpy } = makeSut()
     expect(loadBarbecueListSpy.callsCount).toBe(1)
+    await waitFor(() => screen.getByTestId('barbecue-list'))
+  })
+
+  test('Should render BarbecueItems on success', async () => {
+    makeSut()
+    const barbecueList = screen.getByTestId('barbecue-list')
+    await waitFor(() => barbecueList)
+    expect(barbecueList.querySelectorAll('li.barbecueItemWrap')).toHaveLength(2)
   })
 })
