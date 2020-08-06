2dc42de659a15a667b41c58b32481324f30c4b3b
luan.lazzari <luanlazzari@hotmail.com>
2020-08-06 01:18:33 -0300

    test: ensure BarbecueList render error on failure

--- src/presentation/pages/barbecue-list/barbecue-list.spec.tsx
+++ src/presentation/pages/barbecue-list/barbecue-list.spec.tsx
@@ -4,6 +4,7 @@ import { BarbecueList } from '@/presentation/pages'
 import { LoadBarbecueList } from '@/domain/usecases/load-barbecue-list'
 import { BarbecueModel } from '@/domain/models'
 import { mockBarbecuesModel } from '@/domain/test'
+import { UnexpectedError } from '@/domain/errors'
 
 class LoadBarbecueListSpy implements LoadBarbecueList {
   callsCount: number = 0
@@ -19,8 +20,7 @@ type SutTypes = {
   loadBarbecueListSpy: LoadBarbecueListSpy
 }
 
-const makeSut = (): SutTypes => {
-  const loadBarbecueListSpy = new LoadBarbecueListSpy()
+const makeSut = (loadBarbecueListSpy = new LoadBarbecueListSpy()): SutTypes => {
   render(
     <BarbecueList loadBarbecueList={loadBarbecueListSpy} />
   )
@@ -34,6 +34,7 @@ describe('BarbecueList Component', () => {
     makeSut()
     const barbecueList = screen.getByTestId('barbecue-list')
     expect(barbecueList.querySelectorAll('li:empty')).toHaveLength(3)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
     await waitFor(() => barbecueList)
   })
 
@@ -48,5 +49,16 @@ describe('BarbecueList Component', () => {
     const barbecueList = screen.getByTestId('barbecue-list')
     await waitFor(() => barbecueList)
     expect(barbecueList.querySelectorAll('li.barbecueItemWrap')).toHaveLength(2)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+  })
+
+  test('Should render error on failure', async () => {
+    const loadBarbecueListSpy = new LoadBarbecueListSpy()
+    const error = new UnexpectedError()
+    jest.spyOn(loadBarbecueListSpy, 'loadAll').mockRejectedValueOnce(error)
+    makeSut(loadBarbecueListSpy)
+    await waitFor(() => screen.getByTestId('barbecue-list'))
+    expect(screen.queryByTestId('barbecue-list')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('error')).toHaveTextContent(error.message)
   })
 })
