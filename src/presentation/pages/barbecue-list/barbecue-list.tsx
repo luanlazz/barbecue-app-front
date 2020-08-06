5b0aa9cd855c77e134cf72e5c2bd60ceeecb6df0
luan.lazzari <luanlazzari@hotmail.com>
2020-08-06 01:05:14 -0300

    feat: ensure BarbecueList render BarbecueItems on success

--- src/presentation/pages/barbecue-list/barbecue-list.tsx
+++ src/presentation/pages/barbecue-list/barbecue-list.tsx
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './barbecue-list-styles.scss'
 import { Header } from '@/presentation/components'
-import { BarbecueItemEmpty } from '@/presentation/pages/barbecue-list/components'
+import { BarbecueItemEmpty, BarbecueItem } from '@/presentation/pages/barbecue-list/components'
 import { LoadBarbecueList } from '@/domain/usecases'
+import { BarbecueModel } from '@/domain/models'
 
 type Props = {
   loadBarbecueList: LoadBarbecueList
 }
 
 const BarbecueList: React.FC<Props> = ({ loadBarbecueList }: Props) => {
+  const [state, setState] = useState({
+    barbecues: [] as BarbecueModel[]
+  })
+
   useEffect(() => {
-    (async function () {
-      await loadBarbecueList.loadAll()
-    })()
+    loadBarbecueList.loadAll()
+      .then(barbecues => setState({ barbecues }))
   }, [])
 
   return (
@@ -22,7 +26,12 @@ const BarbecueList: React.FC<Props> = ({ loadBarbecueList }: Props) => {
 
       <div className={Styles.contentWrap}>
         <ul data-testid='barbecue-list'>
-          <BarbecueItemEmpty />
+          {state.barbecues.length
+            ? state.barbecues.map(barbecue => (
+              <BarbecueItem key={barbecue.id} barbecue={barbecue} />
+            ))
+            : <BarbecueItemEmpty />
+          }
         </ul>
       </div>
 
