040f7f20b443039c733401de6041263da56cc2f3
luan.lazzari <luanlazzari@hotmail.com>
2020-08-05 22:44:51 -0300

    test: ensure BarbecueList calls LoadBarbecueList

--- src/presentation/pages/barbecue-list/barbecue-list.spec.tsx
+++ src/presentation/pages/barbecue-list/barbecue-list.spec.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { BarbecueList } from '@/presentation/pages'
+import { LoadBarbecueList } from '@/domain/usecases/load-barbecue-list'
+import { BarbecueModel } from '@/domain/models'
 
-const makeSut = (): void => {
-  render(<BarbecueList />)
+class LoadBarbecueListSpy implements LoadBarbecueList {
+  callsCount: number=0
+
+  async loadAll (): Promise<BarbecueModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadBarbecueListSpy: LoadBarbecueListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadBarbecueListSpy = new LoadBarbecueListSpy()
+  render(
+    <BarbecueList loadBarbecueList={loadBarbecueListSpy} />
+  )
+  return {
+    loadBarbecueListSpy
+  }
 }
 
 describe('BarbecueList Component', () => {
@@ -11,5 +32,10 @@ describe('BarbecueList Component', () => {
     makeSut()
     const barbecueList = screen.getByTestId('barbecue-list')
     expect(barbecueList.querySelectorAll('li:empty').length).toBe(3)
+  })
+
+  test('Should call LoadBarbecueList', () => {
+    const { loadBarbecueListSpy } = makeSut()
+    expect(loadBarbecueListSpy.callsCount).toBe(1)
   })
 })
