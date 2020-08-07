15fe4dda33d851ad8dd44cb71cbfee3da05aee47
luan.lazzari <luanlazzari@hotmail.com>
2020-08-06 22:01:46 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

---
+++ src/main/factories/decorators/authorize-http-get-client-decorator.ts
@@ -0,0 +1,8 @@
+import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
+import { makeLocalStorageAdapter } from '@/main/factories/cache/local-storage-adapter-factory'
+import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
+import { HttpGetClient } from '@/data/protocols/http'
+
+export const makeAuthorizeHttpGetClientDecorator = (): HttpGetClient => {
+  return new AuthorizeHttpGetClientDecorator(makeLocalStorageAdapter(), makeAxiosHttpClient())
+}
