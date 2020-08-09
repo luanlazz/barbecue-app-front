9b3f9ee67629423e471bcbf09b964e6c62e5f9fa
luan.lazzari <luanlazzari@hotmail.com>
2020-08-09 17:14:59 -0300

    test: ensure participants view render participants on success

--- src/presentation/pages/participants-list/participants.spec.tsx
+++ src/presentation/pages/participants-list/participants.spec.tsx
@@ -28,4 +28,12 @@ describe('ParticipantsList Component', () => {
     expect(loadParticipantsListSpy.callsCount).toBe(1)
     await waitFor(() => screen.getByTestId('participants-list'))
   })
+
+  test('Should render ParticipantsItems on success', async () => {
+    makeSut()
+    const barbecueList = screen.getByTestId('participants-list')
+    await waitFor(() => barbecueList)
+    expect(barbecueList.querySelectorAll('tr')).toHaveLength(2)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+  })
 })
