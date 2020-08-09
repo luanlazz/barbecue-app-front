3d653468bf5a2da9245c25c1937f63b798892e75
luan.lazzari <luanlazzari@hotmail.com>
2020-08-09 15:20:43 -0300

    feat: ensure RemoteLoadParticipantsList return a participant list on success

--- src/data/usecases/participants/load-barbecue-list/remote-load-participants-list.ts
+++ src/data/usecases/participants/load-barbecue-list/remote-load-participants-list.ts
@@ -15,7 +15,7 @@ export class RemoteLoadParticipantsList implements LoadParticipantsList {
     })
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return null
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.noContent: return []
       default: throw new UnexpectedError()
     }
