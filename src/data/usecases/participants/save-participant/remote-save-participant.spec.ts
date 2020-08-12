7fcbb5c7dd6d2096d9a32dd4d6f5302c17d621b1
luan.lazzari <luanlazzari@hotmail.com>
2020-08-12 18:33:10 -0300

    test: ensure RemoteSaveParticipant throw UnexpectedError if HttpClient return 400

--- src/data/usecases/participants/save-participant/remote-save-participant.spec.ts
+++ src/data/usecases/participants/save-participant/remote-save-participant.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientSpy } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
 import { SaveParticipant } from '@/domain/usecases'
 import { mockParticipantParams } from '@/domain/test'
+import { UnexpectedError } from '@/domain/errors'
 import faker from 'faker'
 
 type SutTypes = {
@@ -36,5 +37,14 @@ describe('RemoteSaveParticipant', () => {
     const participantParams = mockParticipantParams()
     await sut.save(participantParams)
     expect(httpClientSpy.body).toBe(participantParams)
+  })
+
+  test('Should throw UnexpectedError if HttpClient return 400', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.save(mockParticipantParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
