44eea75ffb26e20a8b85fa138c26bdd0276362c6
luan.lazzari <luanlazzari@hotmail.com>
2020-08-12 00:36:21 -0300

    chore: add fontawesome

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,6 +1,8 @@
 import React, { memo } from 'react'
 import Styles from './header-styles.scss'
 import { useLogout } from '@/presentation/hooks'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 type Props = {
   buttonExit?: boolean
@@ -12,7 +14,10 @@ const Header: React.FC<Props> = ({ buttonExit }: Props) => {
   return (
     <header data-testid='header' className={Styles.header}>
       <h2>Agenda de Churras</h2>
-      {buttonExit && <div data-testid='logout' className={Styles.exit} onClick={logout} />}
+      {buttonExit &&
+        <div data-testid='logout' className={Styles.exit} onClick={logout}>
+          <FontAwesomeIcon icon={faSignOutAlt} size="2x" />
+        </div>}
     </header>
   )
 }
