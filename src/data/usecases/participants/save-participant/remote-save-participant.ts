eeb1ca08227e7fd43b7ca2f04444cbafc4a65bc0
luan.lazzari <luanlazzari@hotmail.com>
2020-08-12 18:24:29 -0300

    feat: ensure RemoteSaveParticipant calls HttpClient with correct url and method

---
+++ src/data/usecases/participants/save-participant/remote-save-participant.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@/data/protocols/http'
+import { SaveParticipant } from '@/domain/usecases'
+
+export class RemoteSaveParticipant implements SaveParticipant {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpClient<SaveParticipant.Model>
+  ) {}
+
+  async save (participant: SaveParticipant.Params): Promise<SaveParticipant.Model> {
+    await this.httpClient.request({
+      url: this.url,
+      method: 'put'
+    })
+    return null
+  }
+}
