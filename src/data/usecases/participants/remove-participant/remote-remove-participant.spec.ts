3e8f6fe88a87af6b40db913869c71392ba38caac
luan.lazzari <luanlazzari@hotmail.com>
2020-08-14 01:07:49 -0300

    test: ensure RemoteRemoveParticipant throw UnexpectedError if return 400

--- src/data/usecases/participants/remove-participant/remote-remove-participant.spec.ts
+++ src/data/usecases/participants/remove-participant/remote-remove-participant.spec.ts
@@ -1,6 +1,7 @@
 import { RemoteRemoveParticipant } from './remote-remove-participant'
 import { HttpClientSpy } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
+import { UnexpectedError } from '@/domain/errors'
 import faker from 'faker'
 
 type SutTypes = {
@@ -11,7 +12,7 @@ type SutTypes = {
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpClientSpy = new HttpClientSpy<void>()
   httpClientSpy.response = {
-    statusCode: HttpStatusCode.ok
+    statusCode: HttpStatusCode.noContent
   }
   const sut = new RemoteRemoveParticipant(url, httpClientSpy)
   return {
@@ -27,5 +28,14 @@ describe('RemoteRemoveParticipant', () => {
     await sut.remove()
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.method).toBe('put')
+  })
+
+  test('Should throw UnexpectedError if HttpClient return 400', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const promise = sut.remove()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
